3ba39acfaf9517458d37da279f068a9343b8aa9c
Koul <dongik0122@gmail.com>
2024-03-29 17:07:36 +0900

    queryDSL
    queryDSL을 활용하여 게시글, 댓글의 검색기능 구현

--- src/main/java/com/toMist/toMist/repository/ArticleCommentRepository.java
+++ src/main/java/com/toMist/toMist/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.toMist.toMist.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
+import com.toMist.toMist.domain.Article;
 import com.toMist.toMist.domain.ArticleComment;
+import com.toMist.toMist.domain.QArticle;
+import com.toMist.toMist.domain.QArticleComment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment>
+{
+  @Override
+  default void customize(QuerydslBindings bindings, QArticleComment root){
+    bindings.excludeUnlistedProperties(true);
+    bindings.including(root.content, root.createdAt, root.createdBy);
+    bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+    bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+    bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+  }
 }
